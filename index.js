f8e1a3edd790f87eff38db65e210a6c0013aedb4
BrayanLuceroM <163746516+BrayanLuceroM@users.noreply.github.com>
2025-08-07 19:25:16 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click', function() {
-    alert('auuu baby i m preying on you tonight')
+    alert('auuu baby i m preying on you tonight 🎵')
 });
 
 const noBtn = document.querySelector('#noBtn');
@@ -12,4 +12,5 @@ noBtn.addEventListener('mouseover', function() {
     noBtn.style.setProperty('top', randomY + '%');
     noBtn.style.setProperty('left', randomX + '%');
     noBtn.style.setProperty('transform', `translate(-${randomX}%,-${randomY}%)`);
+
 })
